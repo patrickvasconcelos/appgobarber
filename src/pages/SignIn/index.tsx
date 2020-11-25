c3971100f6a57c62b4b45141022587b1531e2d5f
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-11-25 00:20:04 -0300

    [NEW] criação de components Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('click');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
 };
 
 export default SignIn;
